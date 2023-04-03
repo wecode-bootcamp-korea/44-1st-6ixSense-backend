083ef5b6415bb3bfc1c3d9b9cd634d230a8c2333
naturerest <asdqwe747@gmail.com>
2023-04-03 18:27:50 +0900

    6ix-sense-초기환경셋팅

--- app.js
+++ app.js
@@ -3,17 +3,31 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
+const dataSource = require('./models/appDataSource');
+const { globalErrorHandler } = require('./utils/error');
+
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(morgan('dev'));
+
+app.use(globalErrorHandler);
 
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, async () => {
+  await dataSource
+    .initialize()
+    .then(() => {
+      console.log('Data Source has been initialized!');
+    })
+    .catch((error) => {
+      console.error('Error during Data Source initialization', error);
+    });
+
   console.log(`Listening to request on port: ${PORT}`);
 });
--- models/appDataSource.js
+++ models/appDataSource.js
@@ -1,4 +1,4 @@
-const { DataSource } = require("typeorm");
+const { DataSource } = require('typeorm');
 
 const appDataSource = new DataSource({
   type: process.env.DB_CONNECTION,
@@ -8,15 +8,5 @@ const appDataSource = new DataSource({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
-
-appDataSource
-  .initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.log("Error occurred during Data Source initialization", err);
-    appDataSource.destroy();
-  });
 
 module.exports = appDataSource;
