3d3faa16db23caaef6826575623e447cb616756b
Mac <mac@Macui-MacBookPro.local>
2023-04-04 17:05:34 +0900

    dbmate 파일수정

--- db/migrations/20230404061501_create_order_items_table.sql
+++ db/migrations/20230404061501_create_order_items_table.sql
@@ -4,7 +4,8 @@ CREATE TABLE order_items
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   order_id INT NOT NULL,
   product_id INT NOT NULL,
-  quantity INT NOT NULL
+  quantity INT NOT NULL,
+  FOREIGN KEY(product_id) REFERENCES products(id)
 );
 
 -- migrate:down
