a18b6bf23ed3bd9b3e72bbe361a1af7517c000ec
Mac <mac@Macui-MacBookPro.local>
2023-04-05 11:48:36 +0900

    [박세익][ADD]제품_카테고리별_상품정보API_수정

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,8 +2,8 @@ const productService = require('../services/productServices');
 const { catchAsync } = require('../utils/error');
 
 const productList = catchAsync(async (req, res) => {
-  const { categoriesId } = req.query;
-  const products = await productService.productList(categoriesId);
+  const { categoryId } = req.query;
+  const products = await productService.productList(categoryId);
   return res.status(200).json({ data: products });
 });
 
