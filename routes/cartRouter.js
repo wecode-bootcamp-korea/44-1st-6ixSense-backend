7ace16a1f0146b3ad1ac34fccb9d487edf986cbd
naturerest <asdqwe747@gmail.com>
2023-04-07 20:03:55 +0900

    [44기 김원종][ADD]장바구니 조회기능

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const cartController = require('../controllers/cartController');
 
 router.post('/post', checkLogInToken, cartController.insertCart);
-router.get('/get', checkLogInToken, cartController.getCartByUserId);
+router.get('/list', checkLogInToken, cartController.getCartByUserId);
 
 module.exports = { router };
