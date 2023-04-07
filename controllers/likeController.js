30f327fbc1fb4586d9cba230ae2a2e0593e2e616
songsong95 <blinglbling137@gmail.com>
2023-04-07 17:28:37 +0900

    feature/likes 추가 삭제 기능 (수정3)

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,12 +1,21 @@
 const likeService = require('../services/likeService');
 const { catchAsync } = require('../utils/error');
+const appDataSource = require('../models/appDataSource');
 
 const createLike = catchAsync(async (req, res) => {
   const { productId } = req.params;
   const userId = await req.user.id;
 
-  const likes = await likeService.createLike(userId, productId);
-  return res.status(201).json({ message: 'Success Create' });
+  const presence = await appDataSource.query(`SELECT products.id FROM products`);
+  const aa = presence[0].id;
+  const string = String(aa);
+
+  if (productId === string) {
+    await likeService.createLike(userId, productId);
+    return res.status(201).json({ message: 'Success Create' });
+  } else {
+    return res.status(404).json({ message: '존재하지 않는 상품 입니다!' });
+  }
 });
 
 const deleteLike = catchAsync(async (req, res) => {
