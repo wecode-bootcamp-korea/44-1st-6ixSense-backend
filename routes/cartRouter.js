3affab7d47ef8d4dd57ab2329c41619d57fe3211
naturerest <asdqwe747@gmail.com>
2023-04-07 09:43:55 +0900

    [44기 김원종][ADD]장바구니 데이터조회 기능추가

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -4,7 +4,7 @@ const checkLogInToken = require('../middleware/auth');
 const router = express.Router();
 const cartController = require('../controllers/cartController');
 
-router.post('', checkLogInToken, cartController.insertCart);
-router.get('', checkLogInToken, cartController.getCartByUserId);
+router.post('/post', checkLogInToken, cartController.insertCart);
+router.get('/get', checkLogInToken, cartController.getCartByUserId);
 
 module.exports = { router };
