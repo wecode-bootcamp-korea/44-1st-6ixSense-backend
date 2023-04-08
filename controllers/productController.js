8b2b395970fb0ba14eb6ae0ebfc7da4ebad1ab5c
Mac <mac@Macui-MacBookPro.local>
2023-04-08 21:41:55 +0900

    [박세익][ADD]Products List and Filtering 추가 수정

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,7 +4,7 @@ const { catchAsync } = require('../utils/error');
 const productList = catchAsync(async (req, res) => {
   const { categoryId, sort, limit = 9, offset = 0 } = req.query;
   const products = await productService.productList(categoryId, sort, parseInt(limit), parseInt(offset));
-  return res.status(200).json({ data: products });
+  return res.status(200).json(products);
 });
 
 module.exports = {
