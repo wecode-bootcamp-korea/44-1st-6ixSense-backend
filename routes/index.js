0176fb6ff9d38efa6c03e0ec365a51949c0a46dc
songsong95 <blinglbling137@gmail.com>
2023-04-13 15:06:00 +0900

    결제 페이지 유저 정보 (수정6)

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,12 @@ const router = express.Router();
 const userRouter = require('./userRouter');
 const productRouter = require('./productRouter');
 const likeRouter = require('./likeRouter');
-const userInformationRouter = require('./userInformationRouter');
 const cartRouter = require('./cartRouter');
 const orderRouter = require('./orderRouter');
 
 router.use('/users', userRouter.router);
 router.use('/products', productRouter.router);
 router.use('/likes', likeRouter.router);
-router.use('/userInfo', userInformationRouter.router);
 router.use('/carts', cartRouter.router);
 router.use('/order', orderRouter.router);
 
