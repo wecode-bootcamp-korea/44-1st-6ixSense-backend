a873e838ed937ef402fc34bcc47bdc7636cbb5f7
songsong95 <blinglbling137@gmail.com>
2023-04-13 14:25:25 +0900

    결제 페이지 유저 정보 (수정5)

--- controllers/userInformationController.js
+++ controllers/userInformationController.js
@@ -1,10 +1,10 @@
-const userInformationService = require('../services/userInformationService');
+const userInformationService = require('../services/userService');
 const { catchAsync } = require('../utils/error');
 
 const userInformation = catchAsync(async (req, res) => {
   const userId = req.user.id;
 
-  const userInfo = await userInformationService.userInformation(userId);
+  const userInfo = await userInformationService.getUserById(userId);
   return res.status(200).json(userInfo);
 });
 
--- services/userInformationService.js
+++
@@ -1,9 +0,0 @@
-const userInformationDao = require('../models/userInformationDao');
-
-const userInformation = async (userId) => {
-  return await userInformationDao.userInformation(userId);
-};
-
-module.exports = {
-  userInformation,
-};
